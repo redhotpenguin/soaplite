080d7858ac5cb42ace824c4f5156728c060d830c
Jan Seidl <seidl@avast.com>
2017-06-19 13:59:48 +0200

    noindex URI::tcp module

--- lib/SOAP/Transport/TCP.pm
+++ lib/SOAP/Transport/TCP.pm
@@ -21,7 +21,8 @@ use IO::SessionData;
 
 # ======================================================================
 
-package URI::tcp; # ok, let's do 'tcp://' scheme
+package 
+    URI::tcp; # ok, let's do 'tcp://' scheme
 
 our $VERSION = 0.715;
 
